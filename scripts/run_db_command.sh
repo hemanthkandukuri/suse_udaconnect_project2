b66fd810973334532160b3555cc723852eba9bda
hemanthkandukuri <hemanth.kandukuri@gmail.com>
2021-10-21 18:38:40 +0530

    namespace string change

--- scripts/run_db_command.sh
+++ scripts/run_db_command.sh
@@ -3,7 +3,7 @@
 # Set database configurations
 export CT_DB_USERNAME=ct_admin
 export CT_DB_NAME=geoconnections
-namespace=udaconnect
+namespace="-n udaconnect"
 
 cat ./db/2020-08-15_init-db.sql | kubectl exec $namespace -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
 
