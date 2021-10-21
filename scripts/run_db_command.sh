a0947917098f53e976dd5fd0f8f8f76a373cc1ee
hemanthkandukuri <hemanth.kandukuri@gmail.com>
2021-10-21 17:41:55 +0530

    added namespace to db scripts

--- scripts/run_db_command.sh
+++ scripts/run_db_command.sh
@@ -3,10 +3,10 @@
 # Set database configurations
 export CT_DB_USERNAME=ct_admin
 export CT_DB_NAME=geoconnections
+namespace=udaconnect
 
+cat ./db/2020-08-15_init-db.sql | kubectl exec $namespace -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
 
-cat ./db/2020-08-15_init-db.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+cat ./db/udaconnect_public_person.sql | kubectl exec $namespace -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
 
-cat ./db/udaconnect_public_person.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
-
-cat ./db/udaconnect_public_location.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+cat ./db/udaconnect_public_location.sql | kubectl exec $namespace -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
