5e145345d6f321d2b1c6b8fdcb4d9adcc736dee6
hemanthkandukuri <hemanth.kandukuri@gmail.com>
2021-10-21 19:41:58 +0530

    changes to routes config

--- modules/persons_api_rest/app/__init__.py
+++ modules/persons_api_rest/app/__init__.py
@@ -7,8 +7,8 @@ db = SQLAlchemy()
 
 
 def create_app(env=None):
-    from modules.persons_api_rest.app.config import config_by_name
-    from modules.persons_api_rest.app.routes import register_routes
+    from ..app.config import config_by_name
+    from ..app.routes import register_routes
 
     app = Flask(__name__)
     app.config.from_object(config_by_name[env or "test"])
